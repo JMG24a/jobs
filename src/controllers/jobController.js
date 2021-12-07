8474b338102c4db970efa34ab201721702c3a13c
jmg24a <jmg24a@gmail.com>
2021-12-07 03:46:33 -0400

    se modifico y añadio ruta post /

--- src/controllers/jobController.js
+++ src/controllers/jobController.js
@@ -1,22 +1,39 @@
 const jobController = {}
 
 jobController.home = (req,res) =>{
-    req.getConnection((err,conn)=>{
+  req.getConnection((err,conn)=>{
+    if(err){
+      console.log(err);
+    }
+    else{
+      conn.query('SELECT * FROM user', (err, rows) =>{
         if(err){
           console.log(err);
         }
-        else{
-          conn.query('SELECT * FROM user', (err, rows) =>{
-            if(err){
-              console.log(err);
-            }
-            console.log(rows)
-            res.render('../views/home.pug',{
-                data: rows,
-            });
-          });
+        console.log(rows)
+        res.render('../views/home.pug',{
+            data: rows,
+        });
+      });
+    }
+  })
+}
+
+jobController.insert = (req,res) =>{
+  const data = req.body;
+  req.getConnection((err,conn)=>{
+    if(err){
+      res.send(err)
+    }else{
+      conn.query('INSERT INTO user set ?', [data] ,(err,rows)=>{
+        if(err){
+          res.send(err)
+        }else{
+          res.redirect('/home');
         }
       })
+    }
+  })
 }
 
 module.exports = jobController;
