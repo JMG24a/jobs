3d6855c7ce09ab0b21cd1ad83ad59cbb03460072
jmg24a <jmg24a@gmail.com>
2021-12-07 03:50:10 -0400

    se corrigio error de syntax en las rutas estaticas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //iniciar entorno
 const express = require('express'); // iniciar express
 const app = express();
+const path = require('path');
 const morgan = require('morgan'); //visualizar las peticiones
 const mysql = require('mysql'); // db para la aplicaion
 const exp_mysql_connection = require('express-myconnection')
@@ -10,7 +11,7 @@ const webpackDevMiddleware = require('webpack-dev-middleware');//sirve para cone
 /*production*/ //const webpackConfig = require('./webpack.config');
 const routerJob = require('./src/router/index.js');
 const routerJobApi = require('./src/router/api'); //asignando la direccion del router
-
+const DBKey = require('./env')
 
 
 //configuracion del server
@@ -18,7 +19,7 @@ app.set('port', process.env.PORT || 3000); //asignar puerto de la app
 app.set('json space', 2); //sive para acomodar el Json con una identacion
 
 app.use(webpackDevMiddleware(webpack(webpackConfig))); //asignacion de la configuracion de webpack para minificar
-app.use(express.static(__dirname + './public' ));//carpeta publica para las vistas "si llegase a tener" 
+app.use(express.static('./public'));//direccion de archivos estaticos" 
 app.use(express.urlencoded({extended: false}));//envios de formatos simples
 app.use(express.json());//expresamos que estaremos enviando json`s
 
@@ -27,15 +28,9 @@ app.use(express.json());//expresamos que estaremos enviando json`s
 
 //middlewares
 app.set('view engine', 'pug');
-app.set('views', './src/views');
+app.set('views', path.join(__dirname, 'src/views'));
 app.use(morgan('dev'));//sirve para ver la peticiones "http" a la app
-app.use(exp_mysql_connection(mysql,{
-    host: 'localhost', 
-    user:'jmg24a', 
-    database: 'jobsDB',
-    password: 'Mysql.jmgy24',
-    connectionLimit: 5
-},'single'));
+app.use(exp_mysql_connection(mysql,DBKey,'single'));
 
 
 
